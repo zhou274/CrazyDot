b5ee515f0f7c43d0addee3d4c1062eac6e9a63f7
zhou274 <274891948@qq.com>
2024-12-30 16:59:32 +0800

    2.0 做好回传和侧边栏

---
+++ crazydot/Assets/Scripts/GameManager.cs
@@ -0,0 +1,248 @@
+using System.Collections;
+using UnityEngine;
+using TTSDK.UNBridgeLib.LitJson;
+using TTSDK;
+using StarkSDKSpace;
+using System.Collections.Generic;
+using UnityEngine.Analytics;
+
+public class GameManager : MonoBehaviour
+{
+	public UIManager uIManager;
+
+	public ScoreManager scoreManager;
+
+	[Space(5f)]
+	public GameObject player;
+
+	[Header("Game settings")]
+	[Space(5f)]
+	public Material trailMaterial;
+
+	[Space(5f)]
+	public Color[] colorTable;
+
+	[Space(5f)]
+	public GameObject obstaclePrefab;
+
+	[Space(5f)]
+	public float minTimeBetweenObstacles = 0.5f;
+
+	public float startTimeBetweenObstacles = 1f;
+
+	private float currentTimeBetweenObstacles;
+
+	private bool spawning;
+
+	private GameObject tempObstacle;
+
+	private Vector2 tempPos;
+
+	private Vector3 screenSize;
+
+	private Color color;
+
+    public string clickid;
+    private StarkAdManager starkAdManager;
+    public static GameManager Instance
+	{
+		get;
+		set;
+	}
+
+	private void Awake()
+	{
+		Object.DontDestroyOnLoad(this);
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			UnityEngine.Object.Destroy(base.gameObject);
+		}
+	}
+
+	private void Start()
+	{
+		Application.targetFrameRate = 60;
+		screenSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+		Color color = colorTable[Random.Range(0, colorTable.Length)];
+		player.GetComponent<Player>().SetColor(color);
+		trailMaterial.color = color;
+	}
+
+	private void Update()
+	{
+		if (uIManager.gameState == GameState.PLAYING && Input.GetMouseButton(0) && !uIManager.IsButton() && !spawning)
+		{
+			spawning = true;
+			ScoreManager.Instance.StartCounting();
+			InitVariables();
+			StartCoroutine(SpawnObstacle());
+		}
+	}
+
+	private void InitVariables()
+	{
+		currentTimeBetweenObstacles = startTimeBetweenObstacles;
+	}
+
+	private IEnumerator SpawnObstacle()
+	{
+		if (ScoreManager.Instance.currentScore > 50f)
+		{
+			currentTimeBetweenObstacles = minTimeBetweenObstacles;
+		}
+		else if (ScoreManager.Instance.currentScore > 35f)
+		{
+			currentTimeBetweenObstacles = startTimeBetweenObstacles - 0.25f;
+		}
+		else if (ScoreManager.Instance.currentScore > 15f)
+		{
+			currentTimeBetweenObstacles = startTimeBetweenObstacles - 0.15f;
+		}
+		tempObstacle = UnityEngine.Object.Instantiate(obstaclePrefab);
+		tempPos = new Vector2(UnityEngine.Random.Range(0f - screenSize.x + obstaclePrefab.GetComponent<SpriteRenderer>().bounds.size.x, screenSize.x - obstaclePrefab.GetComponent<SpriteRenderer>().bounds.size.x), screenSize.y + obstaclePrefab.GetComponent<SpriteRenderer>().bounds.size.y);
+		color = colorTable[Random.Range(0, colorTable.Length)];
+		tempObstacle.GetComponent<Obstacle>().InitObstacle(tempPos, color);
+		yield return new WaitForSecondsRealtime(currentTimeBetweenObstacles);
+		StartCoroutine(SpawnObstacle());
+	}
+
+	public void RestartGame()
+	{
+		if (uIManager.gameState == GameState.PAUSED)
+		{
+			Time.timeScale = 1f;
+		}
+		uIManager.ShowGameplay();
+		ClearScene();
+		scoreManager.ResetCurrentScore();
+	}
+	
+	public void ClearScene()
+	{
+		GameObject[] array = GameObject.FindGameObjectsWithTag("Obstacle");
+		for (int i = 0; i < array.Length; i++)
+		{
+			UnityEngine.Object.Destroy(array[i]);
+		}
+		player.GetComponent<SpriteRenderer>().enabled = true;
+		player.transform.position = new Vector2(0f, -2.5f);
+		color = colorTable[Random.Range(0, colorTable.Length)];
+		player.GetComponent<Player>().SetColor(color);
+		trailMaterial.color = color;
+		player.GetComponent<TrailRenderer>().enabled = true;
+	}
+	public void ContinueGame()
+	{
+        ShowVideoAd("192if3b93qo6991ed0",
+            (bol) => {
+                if (bol)
+                {
+                    if (uIManager.gameState == GameState.GAMEOVER)
+                    {
+                        player.GetComponent<TrailRenderer>().enabled = true;
+                        player.GetComponent<SpriteRenderer>().enabled = true;
+                        uIManager.ContinueGameOver();
+                    }
+
+
+
+                    clickid = "";
+                    getClickid();
+                    apiSend("game_addiction", clickid);
+                    apiSend("lt_roi", clickid);
+
+
+                }
+                else
+                {
+                    StarkSDKSpace.AndroidUIManager.ShowToast("观看完整视频才能获取奖励哦！");
+                }
+            },
+            (it, str) => {
+                Debug.LogError("Error->" + str);
+                //AndroidUIManager.ShowToast("广告加载异常，请重新看广告！");
+            });
+        
+    }
+	public void GameOver()
+	{
+		if (uIManager.gameState == GameState.PLAYING)
+		{
+
+			player.GetComponent<TrailRenderer>().enabled = false;
+			player.GetComponent<SpriteRenderer>().enabled = false;
+			StopAllCoroutines();
+			spawning = false;
+			ScoreManager.Instance.StopCounting();
+			AudioManager.Instance.PlayEffects(AudioManager.Instance.gameOver);
+			uIManager.ShowGameOver();
+			scoreManager.UpdateScoreGameover();
+		}
+	}
+
+
+    public void getClickid()
+    {
+        var launchOpt = StarkSDK.API.GetLaunchOptionsSync();
+        if (launchOpt.Query != null)
+        {
+            foreach (KeyValuePair<string, string> kv in launchOpt.Query)
+                if (kv.Value != null)
+                {
+                    Debug.Log(kv.Key + "<-参数-> " + kv.Value);
+                    if (kv.Key.ToString() == "clickid")
+                    {
+                        clickid = kv.Value.ToString();
+                    }
+                }
+                else
+                {
+                    Debug.Log(kv.Key + "<-参数-> " + "null ");
+                }
+        }
+    }
+
+    public void apiSend(string eventname, string clickid)
+    {
+        TTRequest.InnerOptions options = new TTRequest.InnerOptions();
+        options.Header["content-type"] = "application/json";
+        options.Method = "POST";
+
+        JsonData data1 = new JsonData();
+
+        data1["event_type"] = eventname;
+        data1["context"] = new JsonData();
+        data1["context"]["ad"] = new JsonData();
+        data1["context"]["ad"]["callback"] = clickid;
+
+        Debug.Log("<-data1-> " + data1.ToJson());
+
+        options.Data = data1.ToJson();
+
+        TT.Request("https://analytics.oceanengine.com/api/v2/conversion", options,
+           response => { Debug.Log(response); },
+           response => { Debug.Log(response); });
+    }
+
+
+    /// <summary>
+    /// </summary>
+    /// <param name="adId"></param>
+    /// <param name="closeCallBack"></param>
+    /// <param name="errorCallBack"></param>
+    public void ShowVideoAd(string adId, System.Action<bool> closeCallBack, System.Action<int, string> errorCallBack)
+    {
+        starkAdManager = StarkSDK.API.GetStarkAdManager();
+        if (starkAdManager != null)
+        {
+            starkAdManager.ShowVideoAdWithId(adId, closeCallBack, errorCallBack);
+        }
+    }
+
+
+    
+}
---
+++ crazydot/Assets/Scripts/UIManager.cs
@@ -0,0 +1,162 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using StarkSDKSpace;
+using TTSDK.UNBridgeLib.LitJson;
+using TTSDK;
+
+public class UIManager : MonoBehaviour
+{
+	[Header("GUI Components")]
+	public GameObject mainMenuGui;
+
+	public GameObject pauseGui;
+
+	public GameObject gameplayGui;
+
+	public GameObject gameOverGui;
+
+	public GameState gameState;
+
+	private bool clicked;
+
+    private StarkAdManager starkAdManager;
+
+    public string clickid;
+
+    private void Start()
+	{
+		mainMenuGui.SetActive(value: true);
+		pauseGui.SetActive(value: false);
+		gameplayGui.SetActive(value: false);
+		gameOverGui.SetActive(value: false);
+		gameState = GameState.MENU;
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0) && gameState == GameState.MENU && !clicked)
+		{
+			if (!IsButton())
+			{
+				AudioManager.Instance.PlayEffects(AudioManager.Instance.buttonClick);
+				ShowGameplay();
+			}
+		}
+		else if (Input.GetMouseButtonUp(0) && clicked && gameState == GameState.MENU)
+		{
+			clicked = false;
+		}
+	}
+
+	public void ShowMainMenu()
+	{
+		ScoreManager.Instance.StopCounting();
+		ScoreManager.Instance.ResetCurrentScore();
+		clicked = true;
+		mainMenuGui.SetActive(value: true);
+		pauseGui.SetActive(value: false);
+		gameplayGui.SetActive(value: false);
+		gameOverGui.SetActive(value: false);
+		if (gameState == GameState.PAUSED)
+		{
+			Time.timeScale = 1f;
+		}
+		gameState = GameState.MENU;
+		AudioManager.Instance.PlayEffects(AudioManager.Instance.buttonClick);
+		GameManager.Instance.ClearScene();
+	}
+
+	public void ShowPauseMenu()
+	{
+		if (gameState != GameState.PAUSED)
+		{
+			pauseGui.SetActive(value: true);
+			Time.timeScale = 0f;
+			gameState = GameState.PAUSED;
+			AudioManager.Instance.PlayEffects(AudioManager.Instance.buttonClick);
+		}
+	}
+
+	public void HidePauseMenu()
+	{
+		pauseGui.SetActive(value: false);
+		Time.timeScale = 1f;
+		gameState = GameState.PLAYING;
+		AudioManager.Instance.PlayEffects(AudioManager.Instance.buttonClick);
+	}
+
+	public void ShowGameplay()
+	{
+        ShowInterstitialAd("1lcaf5895d5l1293dc",
+            () => {
+                Debug.LogError("--插屏广告完成--");
+
+            },
+            (it, str) => {
+                Debug.LogError("Error->" + str);
+            });
+        mainMenuGui.SetActive(value: false);
+		pauseGui.SetActive(value: false);
+		gameplayGui.SetActive(value: true);
+		gameOverGui.SetActive(value: false);
+		gameState = GameState.PLAYING;
+		AudioManager.Instance.PlayEffects(AudioManager.Instance.buttonClick);
+		AudioManager.Instance.PlayMusic(AudioManager.Instance.gameMusic);
+	}
+
+	public void ShowGameOver()
+	{
+		mainMenuGui.SetActive(value: false);
+		pauseGui.SetActive(value: false);
+		gameplayGui.SetActive(value: false);
+		gameOverGui.SetActive(value: true);
+		gameState = GameState.GAMEOVER;
+		AudioManager.Instance.PlayMusic(AudioManager.Instance.menuMusic);
+	}
+	//todo
+	public void ContinueGameOver()
+	{
+        mainMenuGui.SetActive(value: false);
+        pauseGui.SetActive(value: false);
+        gameplayGui.SetActive(value: true);
+        gameOverGui.SetActive(value: false);
+        gameState = GameState.PLAYING;
+        AudioManager.Instance.PlayEffects(AudioManager.Instance.buttonClick);
+        AudioManager.Instance.PlayMusic(AudioManager.Instance.gameMusic);
+    }
+
+	public bool IsButton()
+	{
+		bool flag = false;
+		PointerEventData eventData = new PointerEventData(EventSystem.current)
+		{
+			position = UnityEngine.Input.mousePosition
+		};
+		List<RaycastResult> list = new List<RaycastResult>();
+		EventSystem.current.RaycastAll(eventData, list);
+		foreach (RaycastResult item in list)
+		{
+			flag |= (item.gameObject.GetComponent<Button>() != null);
+		}
+		return flag;
+	}
+
+    /// <summary>
+    /// 播放插屏广告
+    /// </summary>
+    /// <param name="adId"></param>
+    /// <param name="errorCallBack"></param>
+    /// <param name="closeCallBack"></param>
+    public void ShowInterstitialAd(string adId, System.Action closeCallBack, System.Action<int, string> errorCallBack)
+    {
+        starkAdManager = StarkSDK.API.GetStarkAdManager();
+        if (starkAdManager != null)
+        {
+            var mInterstitialAd = starkAdManager.CreateInterstitialAd(adId, errorCallBack, closeCallBack);
+            mInterstitialAd.Load();
+            mInterstitialAd.Show();
+        }
+    }
+}
